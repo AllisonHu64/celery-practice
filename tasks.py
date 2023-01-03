eb02aba94df63a9100368636b539d665d7e0db95
Allison Hu <pan.hu46@gmail.com>
2023-01-02 20:09:43 -0500

    Retrieve redis domain from env variable when running in dev mode

--- tasks.py
+++ tasks.py
@@ -1,6 +1,13 @@
 from celery import Celery
+import os
 
-app = Celery('tasks', backend='redis://localhost', broker='redis://localhost//')
+run_env = os.environ.get('RUN_ENV') or 'local'
+if run_env == 'local':
+    app = Celery('tasks', backend='redis://localhost', broker='redis://localhost')
+elif run_env == 'development':
+    redis_domain = os.environ.get('REDIS_DOMAIN')
+    redis_url = 'redis://{}'.format(redis_domain)
+    app = Celery('tasks', backend=redis_url, broker=redis_url)
 
 @app.task
 def add(x, y):
