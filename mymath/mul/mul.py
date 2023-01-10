dd75f2cbf0270303a7ab45e2dfd45fc694b835be
Allison Hu <pan.hu46@gmail.com>
2023-01-09 19:50:36 -0500

    Rewrite mymath as four images

---
+++ mymath/mul/mul.py
@@ -0,0 +1,19 @@
+import sys
+
+def main():
+    if len(sys.argv) <= 2:
+        sys.exit("mul requires at least 2 argument.")
+    
+    try:
+        num1 = int(sys.argv[1])
+    except:
+        sys.exit("arg1 %s is not an interger." % (sys.argv[1]))
+    
+    try:
+        num2 = int(sys.argv[2])
+    except:
+        sys.exit("arg2 %s is not an interger." % (sys.argv[2]))
+    print("result: %d" % (num1 * num2))
+
+if __name__ == '__main__':
+    main()
