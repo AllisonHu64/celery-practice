ef8f03875e676ffcf4f96c2b78c8e443d15ba30f
Allison Hu <pan.hu46@gmail.com>
2023-01-11 23:20:39 -0500

    Intergrate DockerClientHandler to flask application
    - Modify timeout from 1s to 10s for getting celery task result
    - Add message for MymathImageRunFailedException

--- entrypoint.py
+++ entrypoint.py
@@ -1,3 +1,4 @@
+from handler.docker.errors import MymathImageRunFailedException
 from mymath.tasks import add, sub, mul, div
 from flask import Flask
 import os
@@ -8,15 +9,20 @@ flask_app = Flask(__name__)
 def myadd(num1, num2):
     result = add.delay(num1, num2)
     try:
-        return 'Result for add(%d, %d) is %d.\n' % (num1, num2, result.get(timeout=1))
+        return 'Result for add(%d, %d) is %d.\n' % (num1, num2, result.get(timeout=10))
+    except MymathImageRunFailedException:
+        return 'Failed to run add(%d, %d) as image in docker\n' % (num1, num2)
     except Exception as e:
+        print(e)
         return 'Failed to run add(%d, %d) task in celery.\n' % (num1, num2)
 
 @flask_app.route("/sub/<int:num1>/<int:num2>")
 def mysub(num1, num2):
     result = sub.delay(num1, num2)
     try:
-        return 'Result for sub(%d, %d) is %d.\n' % (num1, num2, result.get(timeout=1))
+        return 'Result for sub(%d, %d) is %d.\n' % (num1, num2, result.get(timeout=10))
+    except MymathImageRunFailedException:
+        return 'Failed to run sub(%d, %d) as image in docker\n' % (num1, num2)
     except Exception as e:
         return 'Failed to run sub(%d, %d) task in celery.\n' % (num1, num2)
 
@@ -24,7 +30,9 @@ def mysub(num1, num2):
 def mymul(num1, num2):
     result = mul.delay(num1, num2)
     try:
-        return 'Result for mul(%d, %d) is %d.\n' % (num1, num2, result.get(timeout=1))
+        return 'Result for mul(%d, %d) is %d.\n' % (num1, num2, result.get(timeout=10))
+    except MymathImageRunFailedException:
+        return 'Failed to run mul(%d, %d) as image in docker\n' % (num1, num2)
     except Exception as e:
         return 'Failed to run mul(%d, %d) task in celery.\n' % (num1, num2)
 
@@ -32,6 +40,8 @@ def mymul(num1, num2):
 def mydiv(num1, num2):
     result = div.delay(num1, num2)
     try:
-        return 'Result for div(%d, %d) is %d.\n' % (num1, num2, result.get(timeout=1))
+        return 'Result for div(%d, %d) is %d.\n' % (num1, num2, result.get(timeout=10))
+    except MymathImageRunFailedException:
+        return 'Failed to run div(%d, %d) as image in docker\n' % (num1, num2)
     except Exception as e:
         return 'Failed to run div(%d, %d) task in celery.\n' % (num1, num2)
