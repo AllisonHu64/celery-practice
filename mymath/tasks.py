a983192cefea5ed95707d8e1a3eae40a4ac3b3ee
Allison Hu <pan.hu46@gmail.com>
2023-01-11 23:14:05 -0500

    Intergrate DockerClientHandler to celery application

--- mymath/tasks.py
+++ mymath/tasks.py
@@ -1,17 +1,20 @@
 from .celery import app
+from handler.docker.docker_client_handler import DockerClientHandler
+
+client = DockerClientHandler()
 
 @app.task
 def add(x, y):
-    return x + y
+    return client.run_mymath_add(x, y)
 
 @app.task
 def sub(x, y):
-    return x - y
+    return client.run_mymath_sub(x, y)
 
 @app.task
 def mul(x, y):
-    return x * y
+    return client.run_mymath_mul(x, y)
 
 @app.task
 def div(x, y):
-    return x / y
+    return client.run_mymath_div(x, y)
